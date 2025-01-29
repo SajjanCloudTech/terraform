61b7a7f1d690c6f43f89aefe3a526cf1284c9fee
sazaan <saz08aankhanal@gmail.com>
2025-01-29 16:43:21 +0545

    added changes backend.tf for dev env

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,8 @@
-
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
-    key            = "terraform.tfstate"
-    region         = "us-east-2"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
   }
